ae80974086834cc3c96ac026092e9394ded9b67b
Patipan Kongsirikul <patipan0@hotmail.com>
2026-05-20 18:54:56 +0700

    add allow CORS

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app = FastAPI(title="Stock Analysis API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://*.vercel.app","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
