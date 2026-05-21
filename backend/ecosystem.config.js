2a0be664810505a615559a768f50c691dcfca8b5
Patipan Kongsirikul <patipan0@hotmail.com>
2026-05-21 18:11:57 +0700

    updated ecosystem

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: "portfolio-backend",
-    script: "venv/bin/gunicorn",
+    script: "venv/bin/uvicorn",
     args: "-w 4 -k uvicorn.workers.UvicornWorker main:app --bind 127.0.0.1:8000",
     cwd: "/var/www/vhost/dev-ta.dvrdns.org/backend",
     env: {
